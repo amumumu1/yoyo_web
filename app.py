3d6d54e49c1c75f10115b6c1ed1c67841a6f7670
b24704 <b24704bb@aitech.ac.jp>
2025-08-02 00:11:09 +0900

    s

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ app.config.update(
 
 celery = Celery(
     app.import_name,
-    broker=app.config['broker_url'],
-    backend=app.config['result_backend']
+    broker = app.config['CELERY_BROKER_URL'],
+    backend= app.config['CELERY_RESULT_BACKEND']
 )
 
 celery.conf.update(app.config)
